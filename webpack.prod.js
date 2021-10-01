940ccc2a732638107073b1414a2b220b6188a097
Nikolay Filiuk <Filiuk98@gmail.com>
2021-10-01 17:53:48 +0300

    Build project

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,24 +3,21 @@ const common = require('./webpack.common.js')
 
 module.exports = {
     ...common,
-  mode:'production',
-  devtool: 'source-map',
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            ['@babel/preset-env', {
-              'corejs': '3',
-              'useBuiltIns': 'usage'
-            }]
-          ],
-          plugins: ['@babel/plugin-transform-runtime']
-        }
-      }
-    }]
-  }
+    mode: 'production',
+    devtool: 'source-map',
+    module: {
+        rules:
+            [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /(node_modules|bower_components)/,
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                        plugins: ["@babel/proposal-class-properties"],
+                    },
+                },
+                {test: /\.tsx?$/, loader: "ts-loader"},
+            ],
+    }
 }
